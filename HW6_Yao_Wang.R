fb0b196dcf03d1875d7f17aa43d11fbc64494346
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:48:41 -0400

    Saving HW6_Yao_Wang.R

--- HW6_Yao_Wang.R
+++ HW6_Yao_Wang.R
@@ -1,2 +1,24 @@
 
 clean_data <- raw_data
+# re-use the code from before
+readStates <- function(states)
+{
+    # remover rows that not needed 
+    # -- first row is the total for the US, we do not need that 
+    states <- states[-1,]
+    #-- last row is Puerto Rico, it is not a states
+    num.row <- nrow(states)
+    states <- states[-num.row]
+    
+    # remover the first for coclumns
+    state <- states[,-1:-4]
+    
+    # change names for remaining coclumns
+    colnames(states) <- c("stateName", "population","popOver18", "percentOver18")
+    
+    # return the results
+    return(states)
+}
+
+cleanCencus <- readStates(raw_data)
+str(cleanCencus)
